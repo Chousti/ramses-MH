b8a58c14bcc9862472be2ebfb1d253db5b35a7de
HarleyKatz <harley.b.katz@gmail.com>
2025-08-22 12:45:08 -0500

    bugs

--- hydro/hydro_flag.f90
+++ hydro/hydro_flag.f90
@@ -265,11 +265,12 @@ subroutine stromgren_radius_refine(ind_cell,ok,ncell,ilevel)
   integer::i,indi
   real(dp)::n_strom
   real(dp)::tail_pix
-  real(dp)::dens,tempe,etherm
+  real(dp)::dens,tempe,etherm,ekk
   real(dp)::lam_HI,alpha,r_s,Q
   real(dp)::scale_l,scale_t,scale_d,scale_v,scale_nH,scale_T2
 #if NENER>0
   integer::irad
+  real(dp)::err
 #endif
 #ifdef SOLVERmhd
   real(dp)::emag
@@ -296,33 +297,60 @@ subroutine stromgren_radius_refine(ind_cell,ok,ncell,ilevel)
      ! Calculate the density in H/cc
      dens = max(uold(indi,1),smallr) * scale_nH
 
-     ! Calculate the temperature
-     etherm = uold(indi,neul)
-     etherm = etherm - 0.5d0*uold(indi,2)**2/dens
-#if NDIM > 1 || SOLVERmhd
-     etherm = etherm - 0.5d0*uold(indi,3)**2/dens
-#endif
-#if NDIM > 2 || SOLVERmhd
-     etherm = etherm - 0.5d0*uold(indi,4)**2/dens
-#endif
-#ifdef SOLVERmhd
-     ! the magnetic energy
-     emag =        (uold(indi,6)+uold(indi,nvar+1))**2
-     emag = emag + (uold(indi,7)+uold(indi,nvar+2))**2
-     emag = emag + (uold(indi,8)+uold(indi,nvar+3))**2
-     emag = emag / 8d0
-     etherm = (etherm - emag)
-#endif
+!      ! Calculate the temperature
+!      etherm = uold(indi,neul)
+!      etherm = etherm - 0.5d0*uold(indi,2)**2/dens
+! #if NDIM > 1 || SOLVERmhd
+!      etherm = etherm - 0.5d0*uold(indi,3)**2/dens
+! #endif
+! #if NDIM > 2 || SOLVERmhd
+!      etherm = etherm - 0.5d0*uold(indi,4)**2/dens
+! #endif
+! #ifdef SOLVERmhd
+!      ! the magnetic energy
+!      emag =        (uold(indi,6)+uold(indi,nvar+1))**2
+!      emag = emag + (uold(indi,7)+uold(indi,nvar+2))**2
+!      emag = emag + (uold(indi,8)+uold(indi,nvar+3))**2
+!      emag = emag / 8d0
+!      etherm = (etherm - emag)
+! #endif
+! #if NENER>0
+!      do irad=1,nener
+!         etherm=etherm-uold(indi,nhydro+irad)
+!      end do
+! #endif
+!      ! the temperature
+!      tempe =  etherm / dens * (gamma - 1.0d0)  
+
+!      ! prevent numerical crash due to negative temperature
+!      tempe = max(tempe,smallc**2) * scale_T2
+
+     ! Compute thermal pressure
+     tempe = uold(indi,neul)
+
+     ekk=0.0d0
+     do idim=2,neul-1
+        ekk = ekk + 0.5d0*uold(indi,idim)**2 / MAX(uold(indi,1),smallr)
+     end do
+
+     err = 0.0d0
 #if NENER>0
-     do irad=1,nener
-        etherm=etherm-uold(indi,nhydro+irad)
+     do irad=0,nener-1
+        err = err + uold(indi,inener+irad)
      end do
 #endif
-     ! the temperature
-     tempe =  etherm / dens * (gamma - 1.0d0)  
 
-     ! prevent numerical crash due to negative temperature
-     tempe = max(tempe,smallc**2) * scale_T2
+     emag = 0.0d0
+#ifdef SOLVERmhd
+     do idim=1,3
+        emag = emag + 0.125d0*(uold(indi,idim+neul)+uold(indi,idim+nvar))**2
+     end do
+#endif
+
+     tempe = (gamma-1.0d0) * (tempe - ekk - err - emag)
+
+     ! Compute T2=T/mu in Kelvin
+     tempe = tempe / MAX(uold(indi,1),smallr) * scale_T2
 
      ! Compute the recomination time in s
      lam_HI = 315614d0 / tempe
